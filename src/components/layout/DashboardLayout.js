7b7b8fe6d2eda28b95a1043aef739bb46889640c
Harsh Narwariya <h.narwariya@thesynapses.com>
2026-03-28 14:03:38 +0530

    Redirection Fixes

--- src/components/layout/DashboardLayout.js
+++ src/components/layout/DashboardLayout.js
@@ -51,12 +51,6 @@ export default function DashboardLayout({ user, children }) {
             >
               Workspace
             </Link>
-            <Link
-              href="/profile"
-              className="px-3 py-2 text-sm font-medium text-slate-600 dark:text-slate-300 hover:text-indigo-600 dark:hover:text-indigo-400 hover:bg-indigo-50 dark:hover:bg-indigo-950/50 rounded-lg transition-colors"
-            >
-              Profile
-            </Link>
           </nav>
 
           {/* Right side: theme toggle + user avatar */}
